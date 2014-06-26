bef69bc2147d49d16f27780f58096f676e790e22
Daren David Taylor <daren@niceagency.co.uk>
2014-06-26 13:49:23 +0100

    gps working

--- CruiseControl/CruiseControl/ViewController.swift
+++ CruiseControl/CruiseControl/ViewController.swift
@@ -8,11 +8,76 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import CoreLocation
+
+class ViewController: UIViewController , CLLocationManagerDelegate {
                             
+  @IBOutlet var backgroundImageView: UIImageView
+  @IBOutlet var speedLabel: UILabel
+  
+  var currentSpeed :Double = 0
+  var cruiseSpeed :Double!
+  
+  var manager:CLLocationManager!
+  
+  
+  @IBAction func viewTapped(sender: AnyObject)
+  {
+    if let cruizeSpeedNotNil = cruiseSpeed {
+      cruiseSpeed = nil
+    }
+    else {
+      cruiseSpeed = currentSpeed
+    }
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    manager = CLLocationManager()
+    manager.delegate = self
+    manager.desiredAccuracy = kCLLocationAccuracyBest
+    manager.requestAlwaysAuthorization()
+    manager.startUpdatingLocation()
+  }
+  
+  func locationManager(manager:CLLocationManager, didUpdateLocations locations:AnyObject[]) {
+    println("locations = \(locations)")
+
+    let location = locations[0] as CLLocation
+
+    let metresPerSecond = location.speed
+    
+    let milesPerSecond:CLLocationSpeed =  1609 / metresPerSecond;
+    
+    let milesPerHour = milesPerSecond / 60*60
+    
+    speedLabel.text = "\(milesPerHour)"
+    
+    currentSpeed = milesPerHour;
+    
+    
+    if let cruizeSpeedNotNil = cruiseSpeed {
+    
+    let difference = currentSpeed - cruiseSpeed
+    
+      
+      
+      
+    if difference > 1 {
+      backgroundImageView.image = UIImage(named:"chevron1")
+    }
+    
+      
+      
+      
+      
+    }
+    else {
+      backgroundImageView.image = UIImage(named:"background")
+    }
+    
   }
 
   override func didReceiveMemoryWarning() {
