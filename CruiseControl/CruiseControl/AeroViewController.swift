c74dea0dc047a6c68310dd50c3a4dbee100ace88
Daren David Taylor <daren@niceagency.co.uk>
2014-07-01 16:51:33 +0100

    Top Speedo with overlay

--- CruiseControl/CruiseControl/AeroViewController.swift
+++ CruiseControl/CruiseControl/AeroViewController.swift
@@ -1,36 +1,78 @@
-//
-//  AeroViewController.swift
-//  CruiseControl
-//
-//  Created by Daren taylor on 01/07/2014.
-//  Copyright (c) 2014 LukeBuckless. All rights reserved.
-//
-
 import UIKit
 
-class AeroViewController: UIViewController {
+import CoreLocation
 
+class AeroViewController: UIViewController , CLLocationManagerDelegate {
   
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+  @IBOutlet var speedLabel: UILabel
+  @IBOutlet var timeLabel: UILabel
+  
+  @IBOutlet var speedoOffset: NSLayoutConstraint
+  var currentSpeed :Double = 0
+  var cruiseSpeed :Double!
+  
+  var manager:CLLocationManager!
+  
+  
+  @IBAction func viewTapped(sender: AnyObject)
+  {
+    if let cruizeSpeedNotNil = cruiseSpeed {
+      cruiseSpeed = nil
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    else {
+      cruiseSpeed = currentSpeed
     }
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-
-    /*
-    // #pragma mark - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+    
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    manager = CLLocationManager()
+    manager.delegate = self
+    manager.desiredAccuracy = kCLLocationAccuracyBest
+    manager.requestAlwaysAuthorization()
+    manager.startUpdatingLocation()
+  }
+  
+  
+  
+  func updateTime()
+  {
+    var dateFormatter = NSDateFormatter()
+    dateFormatter.dateFormat = "HH:mm"
+    let d = NSDate()
+    let s = dateFormatter.stringFromDate(d)
+    
+    timeLabel.text = s
+    
+  }
+  
+  func locationManager(manager:CLLocationManager, didUpdateLocations locations:AnyObject[])
+  {
+    
+    println("locations = \(locations)")
+    
+    updateTime()
+    
+    let location = locations[0] as CLLocation
+    
+    currentSpeed = location.speed / 0.44706667;
+    
+    speedLabel.text = String(format:"%.1f",currentSpeed)
+    
+    speedoOffset.constant = Float(0.0-(currentSpeed*6.34))
+  
+    UIView.animateWithDuration(0.5, animations: {
+    
+      self.view.layoutSubviews()
+      
+      })
+    
+  }
 
 }
+
